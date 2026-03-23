6787611ac53b3621293603fa2419ed402d989048
hi4579675 <hi4579675@gmail.com>
2026-03-23 20:37:00 +0900

    feat: 프로젝트 초기 세팅

---
+++ src/test/java/com/fridge2fork/Fridge2forkApplicationTests.java
@@ -0,0 +1,12 @@
+package com.fridge2fork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fridge2forkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
